5013b8c6d4d5d79d9c8fee968cdc161b10a09932
Enderlinq <Enderlinq@users.noreply.github.com>
2020-10-23 17:25:11 +0300

    Initial Commit

---
+++ Fashionize/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fashionize
+//
+//  Created by Wahj Al-duwaisan on 10/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
